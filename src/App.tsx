4b0d8cfd5c0e71634cad3e2f7d8ad35c37bd13be
sabb12 <sabb0923@gmail.com>
2024-04-24 10:23:25 +0900

    feat: header, mypage, register UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Mypage from "./pages/Mypage";
+import Register from "./pages/Register";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="App">
+      {/* <Register /> */}
+      <Header />
+      {/* <Mypage /> */}
+    </div>
+  );
 }
 
 export default App;

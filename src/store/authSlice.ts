2984b149450ede80d63eef9cadbec244d0f359c3
eastorigin <eastorigin21@gmail.com>
2024-05-01 06:02:58 +0900

    Feat: 펫시터 회원가입 작업 중

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -2,13 +2,13 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import {
   LoginState,
   LoginPayload,
+  SignupPayload,
   LoginErrorPayload,
   SignupErrorPayload,
 } from "../types/authTypes";
 
 const initialState: LoginState = {
   userid: "",
-  userpw: "",
   error: {
     userid: "",
     userpw: "",
@@ -23,26 +23,33 @@ export const authSlice = createSlice({
     loginSuccess: (state, action: PayloadAction<LoginPayload>) => {
       state.isLoggedIn = true;
       state.userid = action.payload.userid;
-      state.userpw = "";
       state.error = { userid: "", userpw: "" };
     },
     loginFailure: (state, action: PayloadAction<LoginErrorPayload>) => {
       state.error = { ...state.error, ...action.payload };
     },
+    signupSuccess: (state, action: PayloadAction<SignupPayload>) => {
+      state.isLoggedIn = true;
+      state.userid = action.payload.userid;
+      state.error = { userid: "", userpw: "" };
+    },
     signupFailure: (state, action: PayloadAction<SignupErrorPayload>) => {
-      // 회원가입 실패시 에러 상태 업데이트
       state.error = { ...state.error, ...action.payload };
     },
     logout: (state) => {
       state.isLoggedIn = false;
       state.userid = "";
-      state.userpw = "";
       state.error = { userid: "", userpw: "" };
     },
   },
 });
 
-export const { loginSuccess, loginFailure, signupFailure, logout } =
-  authSlice.actions;
+export const {
+  loginSuccess,
+  loginFailure,
+  signupSuccess,
+  signupFailure,
+  logout,
+} = authSlice.actions;
 
 export default authSlice.reducer;
--- src/types/authTypes.ts
+++ src/types/authTypes.ts
@@ -1,6 +1,5 @@
 export interface LoginState {
   userid: string;
-  userpw: string;
   error: {
     userid: string;
     userpw: string;
@@ -14,7 +13,6 @@ export interface LoginPayload {
 
 export interface SignupPayload {
   userid: string;
-  userpw: string;
   name: string;
   address: string;
   usertype: string; // 회원 유형 정보 추가
@@ -31,5 +29,6 @@ export interface SignupErrorPayload {
   name?: string; // 회원가입 시 이름 입력 에러
   address?: string; // 회원가입 시 주소 입력 에러
   usertype?: string; // 회원가입 시 회원 유형 선택 에러
+  message?: string;
   // 필요에 따라 추가적인 에러 필드를 여기에 포함시킬 수 있습니다.
 }

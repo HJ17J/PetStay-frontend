c04df2edd2e5b18df484ad91eb1b9be5bbca62fd
eastorigin <eastorigin21@gmail.com>
2024-05-03 14:22:57 +0900

    소셜 로그인

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./config/i18n";
 
+console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
